ee399b7f5070bcd2efcea2f2aa05c64b242205d5
jaffar <gamaportal8@gmail.com>
2024-09-18 23:12:02 +0500

    initialize project

---
+++ Angular-Observable/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { filter, map, Observable } from 'rxjs';
+import { Todo } from './todo';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule,HttpClientModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent implements OnInit{
+  list:Todo[] = [];
+  constructor(public httpClient:HttpClient){}
+
+  ngOnInit(): void {
+    this.httpClient.get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+    .pipe(
+      map(todos => todos.filter(a => a.completed)),
+    )
+    .subscribe((data) => {
+      this.list = data;
+    })
+  }
+}
---
+++ Angular-Observable/src/app/todo.ts
@@ -0,0 +1,6 @@
+export interface Todo{
+    userID:BigInteger,
+    id:BigInteger,
+    title:string,
+    completed:boolean
+}
